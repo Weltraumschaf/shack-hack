0af96a01bf58e26bba1244708aba97edd689d9a0
Weltraumschaf <ich@weltraumschaf.de>
2013-02-25 19:49:32 +0100

    Initial commit.

---
+++ src/main/java/de/weltraumschaf/shackhack/Main.java
@@ -0,0 +1,24 @@
+/*
+ * LICENSE
+ *
+ * "THE BEER-WARE LICENSE" (Revision 42):
+ * "Sven Strittmatter" <weltraumschaf@googlemail.com> wrote this file.
+ * As long as you retain this notice you can do whatever you want with
+ * this stuff. If we meet some day, and you think this stuff is worth it,
+ * you can buy me a beer in return.
+ *
+ */
+
+package de.weltraumschaf.shackhack;
+
+/**
+ *
+ * @author Sven Strittmatter <weltraumschaf@googlemail.com>
+ */
+public class Main {
+
+    public static void main(final String[] args) {
+        System.out.println("Hello world!");
+    }
+
+}

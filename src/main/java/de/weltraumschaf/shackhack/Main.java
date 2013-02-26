df5315bffa39b2ff4ac3869c6c325304d400f974
Weltraumschaf <ich@weltraumschaf.de>
2013-02-26 23:18:17 +0100

    Integrate Jasmin.

--- src/main/java/de/weltraumschaf/shackhack/Main.java
+++ src/main/java/de/weltraumschaf/shackhack/Main.java
@@ -13,7 +13,12 @@ package de.weltraumschaf.shackhack;
 import com.google.common.io.Files;
 import de.weltraumschaf.shackhack.antlr.ShackHackParser;
 import de.weltraumschaf.shackhack.antlr.ShackHackLexer;
+import jasmin.ClassFile;
+import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.PrintStream;
+import java.io.StringReader;
 import org.antlr.v4.runtime.ANTLRFileStream;
 import org.antlr.v4.runtime.CharStream;
 import org.antlr.v4.runtime.CommonTokenStream;
@@ -25,44 +30,53 @@ import org.antlr.v4.runtime.TokenStream;
  */
 public class Main {
 
-    private static final String PREAMBLE = ".class public %s%n"
-            + ".super java/lang/Object%n%n"
-            + ".method public static main([Ljava/lang/String;)V%n"
-            + "    .limit stack %d%n"
-            + "    getstatic java/lang/System/out Ljava/io/PrintStream;%n%n";
-    private static final String FOOTER = "%n"
-            + "    invokevirtual java/io/PrintStream/println(I)V%n"
-            + "    return%n.end method%n";
     private static final String ENCODING = "UTF-8";
-    private static final int STACK_SIZE = 10;
+    private static final PrintStream ERR = System.err;
+    private static final PrintStream OUT = System.out;
 
     public static void main(final String[] args) {
+        System.exit(new Main().run(args));
+    }
+
+    public final int run(final String[] args) {
         if (args.length != 1) {
-            System.out.println("Give source as one argument");
+            ERR.println("Give source as one argument");
+            return -1;
         }
 
         final String source = args[0];
-        final StringBuilder buffer = new StringBuilder();
+        final String className = extractClassName(source);
+        String asm = "";
 
         try {
-            buffer.append(generatePreambel(extractClassName(source), STACK_SIZE));
-            buffer.append(indent(parseSource(source)));
-            buffer.append(generateFooter());
+            asm = new Generator().generate(className, parseSource(source));
         } catch (IOException ex) {
-            System.out.println(String.format("Can't read file '%s'!", source));
-            System.exit(1);
+            ERR.println(String.format("Can't read file '%s'!", source));
+            return -2;
         }
 
-        System.out.print(buffer.toString());
-        System.exit(0);
-    }
+        try {
+            final ClassFile classFile = new ClassFile();
+            classFile.readJasmin(new StringReader(asm), className, false);
 
-    static String generatePreambel(final String className, final int stacksize) {
-        return String.format(PREAMBLE, className, stacksize);
-    }
+            if (classFile.errorCount() > 0) {
+                ERR.println("Found " + classFile.errorCount() + " errors");
+                return -3;
+            }
 
-    static String generateFooter() {
-        return String.format(FOOTER);
+            final File outFile = new File(className + ".class");
+            final FileOutputStream outp = new FileOutputStream(outFile);
+            classFile.write(outp);
+            outp.close();
+            OUT.println("Generated: " + outFile.getPath());
+        } catch (IOException ex) {
+            // No IO errors w/ string readers
+        } catch (Exception ex) {
+            ERR.println(ex.getMessage());
+            return -4;
+        }
+
+        return 0;
     }
 
     static String parseSource(final String filename) throws IOException {
@@ -72,16 +86,6 @@ public class Main {
 
         final ShackHackParser parser = new ShackHackParser(tokens);
         return new ByteCodeVisitor().visit(parser.start());
-    }
-
-    static String indent(final String in) {
-        final StringBuilder buffer = new StringBuilder();
-
-        for (final String line : in.split(String.format("%n"))) {
-            buffer.append(String.format("    %s%n", line));
-        }
-
-        return buffer.toString();
     }
 
     static String extractClassName(final String fileName) {

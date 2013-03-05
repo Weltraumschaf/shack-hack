a5107443ec4e324d26cd38b8d6f2fc2e366b84e4
Weltraumschaf <ich@weltraumschaf.de>
2013-03-05 12:47:10 +0100

    Introduce symbol table.

---
+++ src/main/java/de/weltraumschaf/shackhack/SymbolTable.java
@@ -0,0 +1,83 @@
+/*
+ *  LICENSE
+ *
+ * "THE BEER-WARE LICENSE" (Revision 43):
+ * "Sven Strittmatter" <weltraumschaf@googlemail.com> wrote this file.
+ * As long as you retain this notice you can do whatever you want with
+ * this stuff. If we meet some day, and you think this stuff is worth it,
+ * you can buy me a non alcohol-free beer in return.
+ *
+ * Copyright (C) 2012 "Sven Strittmatter" <weltraumschaf@googlemail.com>
+ */
+
+package de.weltraumschaf.shackhack;
+
+import com.google.common.base.Objects;
+import com.google.common.collect.Maps;
+import java.util.Map;
+
+/**
+ *
+ * @author Sven Strittmatter <weltraumschaf@googlemail.com>
+ */
+class SymbolTable {
+
+    static class Entry {
+        final int id;
+        final String name;
+
+        public Entry(int id, String name) {
+            this.id = id;
+            this.name = name;
+        }
+
+        public int getId() {
+            return id;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hashCode(name, id);
+        }
+
+        @Override
+        public boolean equals(final Object obj) {
+            if (!(obj instanceof Entry)) {
+                return false;
+            }
+
+            final Entry other = (Entry) obj;
+            return Objects.equal(name, other.name) && Objects.equal(id, other.id);
+        }
+
+
+
+    }
+
+    private final Map<String, Entry> table = Maps.newHashMap();
+    private int currentSlot;
+
+    public Entry lookup(final String name) {
+        if (table.containsKey(name)) {
+            return table.get(name);
+        }
+
+        return null;
+    }
+
+    public Entry enter(final String name) {
+        if (null != lookup(name)) {
+            throw new IllegalArgumentException(String.format("Symbal with name '%s' already entered in table!", name));
+        }
+
+        final Entry symbol = new Entry(currentSlot, name);
+        table.put(name, symbol);
+        currentSlot++;
+        return symbol;
+    }
+
+}
---
+++ src/test/java/de/weltraumschaf/shackhack/SymbolTableTest.java
@@ -0,0 +1,87 @@
+/*
+ *  LICENSE
+ *
+ * "THE BEER-WARE LICENSE" (Revision 43):
+ * "Sven Strittmatter" <weltraumschaf@googlemail.com> wrote this file.
+ * As long as you retain this notice you can do whatever you want with
+ * this stuff. If we meet some day, and you think this stuff is worth it,
+ * you can buy me a non alcohol-free beer in return.
+ *
+ * Copyright (C) 2012 "Sven Strittmatter" <weltraumschaf@googlemail.com>
+ */
+
+package de.weltraumschaf.shackhack;
+
+import de.weltraumschaf.shackhack.SymbolTable.Entry;
+import org.junit.Test;
+import static org.junit.Assert.assertThat;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.not;
+import static org.hamcrest.core.IsNull.nullValue;
+import static org.hamcrest.core.IsNull.notNullValue;
+import org.junit.Rule;
+import org.junit.rules.ExpectedException;
+
+/**
+ *
+ * @author Sven Strittmatter <weltraumschaf@googlemail.com>
+ */
+public class SymbolTableTest {
+
+    @Rule public ExpectedException thrown = ExpectedException.none();
+    private final SymbolTable sut = new SymbolTable();
+
+    @Test
+    public void equalsOfEntry() {
+        final Entry sut1 = new SymbolTable.Entry(1, "foo");
+        final Entry sut2 = new SymbolTable.Entry(1, "foo");
+        final Entry sut3 = new SymbolTable.Entry(3, "foo");
+        final Entry sut4 = new SymbolTable.Entry(1, "bar");
+
+        assertThat(sut1, is(sut1));
+        assertThat(sut2, is(sut1));
+        assertThat(sut1, is(sut2));
+        assertThat(sut2, is(sut2));
+
+        assertThat(sut3, is(sut3));
+        assertThat(sut4, is(sut4));
+
+        assertThat(sut3, is(not(sut1)));
+        assertThat(sut3, is(not(sut2)));
+        assertThat(sut3, is(not(sut4)));
+
+        assertThat(sut4, is(not(sut1)));
+        assertThat(sut4, is(not(sut2)));
+        assertThat(sut4, is(not(sut3)));
+    }
+
+    @Test
+    public void lookup_returnsNullIfNotEntered() {
+        assertThat(sut.lookup("foo"), nullValue());
+    }
+
+    @Test
+    public void lookup_returnsEntryIfEntered() {
+        sut.enter("foo");
+        final SymbolTable.Entry symbol = sut.lookup("foo");
+        assertThat(symbol, notNullValue());
+        assertThat(symbol.getName(), is("foo"));
+        assertThat(symbol.getId(), is(0));
+    }
+
+    @Test
+    public void enter_throwsExceptionOnEnteringNameTwice() {
+        sut.enter("foo");
+        thrown.expect(IllegalArgumentException.class);
+        thrown.expectMessage("Symbal with name 'foo' already entered in table!");
+        sut.enter("foo");
+    }
+
+    @Test
+    public void enter_eachEntryIncrementsSlot() {
+        assertThat(sut.enter("foo").getId(), is(0));
+        assertThat(sut.enter("bar").getId(), is(1));
+        assertThat(sut.enter("baz").getId(), is(2));
+    }
+
+}
